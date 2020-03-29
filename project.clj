fa066824f375d5c3e8991acb3fd23de08b964149
Aaron Santos <aaronds109@gmail.com>
2020-03-29 10:28:19 -0700

    Add Clojure project files

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject gossamer "0.1.0-SNAPSHOT"
+  :description "BYOR - Build Your Own React Using Clojure"
+  :url "https://github.com/aaron-santos/gossamer"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :repl-options {:init-ns gossamer.core})
